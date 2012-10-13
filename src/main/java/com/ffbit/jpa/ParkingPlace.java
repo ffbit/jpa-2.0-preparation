c4c7f088ee45550e1d1f6236ba464bfbc84772d3
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-10-14 02:21:27 +0300

    ParkingPlace entity

---
+++ src/main/java/com/ffbit/jpa/ParkingPlace.java
@@ -0,0 +1,63 @@
+package com.ffbit.jpa;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "parking_places")
+@Access(AccessType.PROPERTY)
+public class ParkingPlace implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private Integer lot;
+    private String location;
+
+    private ParkingPlace() {
+        super();
+    }
+
+    public ParkingPlace(Integer lot, String location) {
+        this();
+        this.lot = lot;
+        this.location = location;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic(optional = false)
+    public Integer getLot() {
+        return lot;
+    }
+
+    public void setLot(Integer lot) {
+        this.lot = lot;
+    }
+
+    @Column(nullable = false, length = 40)
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
