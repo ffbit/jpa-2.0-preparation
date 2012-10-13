9ddbb731a078672ebfd147b829617e4818abe739
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-10-14 01:54:41 +0300

    Employee entity.

---
+++ src/main/java/com/ffbit/jpa/Employee.java
@@ -0,0 +1,53 @@
+package com.ffbit.jpa;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+@Access(AccessType.PROPERTY)
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private BigDecimal salary;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false, length = 40)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+}
