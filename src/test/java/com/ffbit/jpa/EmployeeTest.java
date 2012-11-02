89ceff8918aa06e5c155dfada5132ed26b53cf1d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-11-03 00:20:57 +0200

    Created a test hierarchy.

--- src/test/java/com/ffbit/jpa/EmployeeTest.java
+++ src/test/java/com/ffbit/jpa/EmployeeTest.java
@@ -1,28 +1,13 @@
+package com.ffbit.jpa;
+
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.ffbit.jpa.Employee;
-import com.ffbit.jpa.ParkingPlace;
-
-@Transactional
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:application-context.xml")
-public class EmployeeTest {
-
-    @PersistenceContext
-    private EntityManager em;
-
+public class EmployeeTest extends AbstractJpaTest {
     private String name;
     private BigDecimal salary;
 
@@ -55,4 +40,5 @@ public class EmployeeTest {
 
         assertNotNull(persisted);
     }
+
 }
