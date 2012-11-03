ebc507dd24a94661b52547ccb8074625962e425e
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-11-04 00:15:02 +0200

    Department test with annotation driven beans.
    Note:
    - they instantiate unexpectedly often.

--- src/test/java/com/ffbit/jpa/DepartmentTest.java
+++ src/test/java/com/ffbit/jpa/DepartmentTest.java
@@ -1,27 +1,59 @@
 package com.ffbit.jpa;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
+@Configuration
 public class DepartmentTest extends AbstractJpaTest {
     private String name;
 
-    @Before
-    public void setUp() throws Exception {
-        name = "Sales Department";
-    }
+    @Autowired
+    private Department department;
+
+    @Autowired
+    private Department persistedDepartment;
 
     @Test
     public void itShouldBePersistable() throws Exception {
-        Department department = new Department(name);
-
         em.persist(department);
 
         Department persisted = em.find(Department.class, department.getId());
 
-        assertNotNull(persisted);
+        assertThat(persisted, is(notNullValue()));
+    }
+
+    @Test
+    public void itShouldAllowUpdateTheName() throws Exception {
+        String newName = "Rebranded Department";
+        persistedDepartment.setName(newName);
+        em.merge(persistedDepartment);
+
+        assertThat(persistedDepartment.getName(), is(equalTo(newName)));
+    }
+
+    @Bean(name = "department")
+    @Scope("prototype")
+    Department instantiateValidDepartment() {
+        name = "Sales Department";
+
+        return new Department(name);
+    }
+
+    @Bean(name = "persistedDepartment")
+    @Scope("prototype")
+    Department persistValidDepartment() {
+        Department dep = instantiateValidDepartment();
+        em.persist(dep);
+
+        return dep;
     }
 
 }
